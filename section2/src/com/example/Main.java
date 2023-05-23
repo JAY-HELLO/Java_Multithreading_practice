9d71a6bc72c0cfb6e6b14ceb74c2a4e0ecd3a644
Double_J <double04j@gmail.com>
2023-05-23 15:53:57 +0900

    스레드 이름 및 우선순위 부여

--- section2/src/com/example/Main.java
+++ section2/src/com/example/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
             System.out.println(" we are now in thread " + Thread.currentThread().getName());
+            System.out.println("Thread priority = " + Thread.currentThread().getPriority());
         });
 
+        thread.setName("새 스레드");
+        thread.setPriority(Thread.MAX_PRIORITY);
         System.out.println(" we are in thread  = " + thread.currentThread().getName() + " before starting a new thread" );
         thread.start();
         System.out.println(" we are in thread  = " + thread.currentThread().getName() + " after starting a new thread" );
