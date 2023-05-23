31a2129700e1f2f577c67673aa01ba74c78c4fb3
Double_J <double04j@gmail.com>
2023-05-23 15:57:52 +0900

    스레드 에러검출

--- section2/src/com/example/Main.java
+++ section2/src/com/example/Main.java
@@ -4,15 +4,17 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
-            System.out.println(" we are now in thread " + Thread.currentThread().getName());
-            System.out.println("Thread priority = " + Thread.currentThread().getPriority());
+            throw new RuntimeException("Internal Exception");
         });
 
-        thread.setName("새 스레드");
-        thread.setPriority(Thread.MAX_PRIORITY);
-        System.out.println(" we are in thread  = " + thread.currentThread().getName() + " before starting a new thread" );
+        thread.setName("오류 스레드");
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println(" critical error happend in thread " + t.getName() + " the error is " + e.getMessage());
+            }
+        });
         thread.start();
-        System.out.println(" we are in thread  = " + thread.currentThread().getName() + " after starting a new thread" );
         Thread.sleep(10000);
 
     }
