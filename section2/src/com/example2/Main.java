684c24c5ea0b0bc0fe0f5b7ed32e46a2a0b25051
Double_J <double04j@gmail.com>
2023-05-24 14:54:56 +0900

    스레드 상속 및 동작확인

--- section2/src/com/example2/Main.java
+++ section2/src/com/example2/Main.java
@@ -1,16 +1,112 @@
 package com.example2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    public static final int MAX_PASSWORD = 9999;
     public static void main(String[] args) {
-        Thread thread = new NewThread();
-        thread.start();
+        Random random = new Random();
+        Vault vault = new Vault(random.nextInt(MAX_PASSWORD));
+
+        List<Thread> threads = new ArrayList<>();
+        threads.add(new AscendingHackerThread(vault));
+        threads.add(new DescendingHackerThread(vault));
+        threads.add(new PoliceThread());
+        for (Thread thread : threads) {
+            thread.start();
+        }
     }
 
-    private static class NewThread extends Thread{
+    /**
+     * 금고 클래스
+     */
+    private static class Vault{
+        private int password;
+
+        public Vault(int password) {
+            this.password = password;
+        }
+
+        public boolean isCorrectPassword(int guess) {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return this.password == guess;
+        }
+    }
+
+    private static abstract class HackerThread extends Thread{
+        protected Vault vault;
+
+        public HackerThread(Vault vault) {
+            this.vault = vault;
+            this.setName(this.getClass().getSimpleName());
+            this.setPriority(Thread.MAX_PRIORITY);
+        }
+        @Override
+        public void start(){
+            System.out.println("starting thread = " + this.getName());
+            super.start();
+        }
+    }
+
+    /**
+     * 낮은 수 부터 비밀번호를 찾는 hacker thread
+     */
+    private static class AscendingHackerThread extends HackerThread{
+        public AscendingHackerThread(Vault vault) {
+            super(vault); // 부모의 vault를 이용해 생성자 초기화.
+        }
         @Override
         public void run(){
-            System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+            for (int guess = 0; guess < MAX_PASSWORD; guess++) {
+                 if(vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + "guessed the password" + guess);
+                    System.exit(0);
+                 }
 
+            }
+        }
+
+        }
+
+    /**
+     * 높은수부터 비밀번호를 찾는 hacker thread
+     */
+    private static class DescendingHackerThread extends HackerThread {
+        public DescendingHackerThread(Main.Vault vault) {
+            super(vault); // 부모의 vault를 이용해 생성자 초기화.
+        }
+        @Override
+        public void run(){
+            for (int guess = MAX_PASSWORD; guess >= 0; guess--) {
+                if(vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + "guessed the password" + guess);
+                    System.exit(0);
+                }
+
+            }
+        }
+
+    }
+
+    private static class PoliceThread extends Thread{
+        @Override
+        public void run(){
+            for (int i = 10; i > 0; i--) {
+                 try{
+                     Thread.sleep(1000);
+                 }catch (InterruptedException e){}
+                 System.out.println(i);
+
+            }
+            System.out.println("Hacker 이 잡혔다.");
+            System.exit(0);
         }
     }
 }
